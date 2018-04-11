8591662ab63ba6a19a7b81309222d11ac3732b16
Henri Menke <henri@icp.uni-stuttgart.de>
2018-04-11 17:20:55 +1200

    Infrastrcture

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+shopt -s nullglob
+set -ex
+
+for dir in *; do
+    if [ -d "${dir}" ]; then
+        cd "${dir}"
+        for file in *.tex; do
+            if [ -f "${file}" ]; then
+                context --nonstopmode "${file}"
+            fi
+        done
+    fi
+done
+
