ea1ec96ec848b6c911254abc72e02409799d0365
Henri Menke <henri@icp.uni-stuttgart.de>
2019-02-18 17:37:28 +1300

    Install dependencies

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Install dependencies
+apt-get update
+apt-get install -yy libgsl-dev
+
+# Fail on errors
+set -e
+
+# Don't fail on empty directories
 shopt -s nullglob
-set -ex
+
+# Tell me what's happening
+set -x
 
 for dir in *; do
     if [ -d "${dir}" ]; then
